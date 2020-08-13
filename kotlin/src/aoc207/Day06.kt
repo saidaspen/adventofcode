c6ddd3c94c1492253319a0bae3f24757e8a7de51
Said Aspen <mail@saidaspen.se>
2020-08-13 13:33:06 +0200

    Added day 6 solutions

--- kotlin/src/aoc207/Day06.kt
+++ kotlin/src/aoc207/Day06.kt
@@ -1,6 +1,7 @@
 package aoc207
 
 import util.getInput
+import util.intsOf
 
 fun main() {
     val input = getInput("201706")
@@ -10,10 +11,57 @@ fun main() {
 
 class Day06 {
     fun part1(input: String): String {
-        return ""
+        val register = intsOf(input)
+        val visitedConfigs = HashSet<String>()
+        var iterations = 0
+        while (true) {
+            iterations++
+            visitedConfigs.add(register.toString())
+            redistribute(register)
+            if (visitedConfigs.contains(register.toString())) return iterations.toString()
+
+        }
+    }
+
+    private fun redistribute(register: MutableList<Int>) {
+        var pos = findMax(register)
+        var bucket = register[pos]
+        register[pos] = 0
+        while (bucket > 0) {
+            pos = (pos + 1) % register.size
+            register[pos]++
+            bucket--
+        }
+    }
+
+    private fun findMax(register: MutableList<Int>): Int {
+        var max = 0
+        var maxIdx = -1
+        for (i in register.indices) {
+            if (register[i] > max) {
+                max = register[i]
+                maxIdx = i
+            }
+        }
+        return maxIdx
     }
 
     fun part2(input: String): String {
-        return ""
+        val register = intsOf(input)
+        val visitedConfigs = HashSet<String>()
+        while (true) {
+            visitedConfigs.add(register.toString())
+            redistribute(register)
+            if (visitedConfigs.contains(register.toString())) {
+                break
+            }
+        }
+        var iterations = 0
+        val loopStart = register.toString()
+        do {
+            redistribute(register)
+            iterations++
+        } while (loopStart != register.toString())
+        return iterations.toString()
     }
 }
