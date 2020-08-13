964ebc4cda6379883508b92047b1df0922999746
Said Aspen <mail@saidaspen.se>
2020-08-13 06:50:32 +0200

    Added solution for day 5

--- kotlin/src/util/Util.kt
+++ kotlin/src/util/Util.kt
@@ -10,6 +10,6 @@ fun digitsOf(input: String): List<Int> {
     return input.filter { it.isDigit() }.map { it.toString().toInt() }.toList()
 }
 
-fun intsOf(input: String): List<Int> {
-    return input.split("[^\\d]+".toRegex()).map { it.toInt() }.toList()
+fun intsOf(input: String): MutableList<Int> {
+    return "-?\\d+".toRegex(RegexOption.MULTILINE).findAll(input).map { it.value.toInt() }.toMutableList()
 }
