b47744ddbbb45a990953f025aebaaeb36e270983
Said Aspen <mail@saidaspen.se>
2020-08-13 13:03:02 +0200

    Added day 3 solution

--- kotlin/src/aoc207/Day03.kt
+++ kotlin/src/aoc207/Day03.kt
@@ -6,6 +6,7 @@ import kotlin.math.pow
 
 fun main() {
     println("Day 3: " + Day03().part1("289326"))
+    println("Day 3: " + Day03().part2("289326"))
 }
 
 class Day03 {
@@ -21,41 +22,46 @@ class Day03 {
         val midOnArm = (corner - (sideLen - 1) / 2) - ((sideLen - 1) * arm)
         val distanceToMid = (midOnArm - target).absoluteValue
         val distanceToLevel = ((sideLen - 1) / 2)
-        println("Target: $target, Corner: $corner, sidelen = $sideLen, steps = $steps, arm = $arm, midOnArm $midOnArm, distanceToMid $distanceToMid, distanceToLevel $distanceToLevel")
         return (distanceToLevel + distanceToMid).toString()
     }
 
-
     fun part2(input: String): String {
         val target = input.toInt()
-        val origin = Pair(0, 0)
         val map = HashMap<Pair<Int, Int>, Int>()
-        map.put(origin, 1)
-        var lvl = 1
-        var currStep = 1
+        val directions = arrayOf(Pair(0,1), Pair(-1, 0), Pair(0, -1), Pair(1, 0))
+
+        var sideLen = 1
         var currPos = Pair(0, 0)
-        var dir = 0
-        var dirs = arrayOf(Pair(1,0), Pair(0,1), Pair(-1,0), Pair(0,-1))
+        var step = 1
+        var direction = 0
         while (true) {
-            map.put(currPos, valueOf(currPos, map))
-            currPos = Pair(currPos.first + dirs[dir].first, currPos.second + dirs[dir].second)
-
-            var stepsInLevel = (lvl + 2).toDouble().pow(2) - lvl.toDouble().pow(2)
-
-
-
+            val value = valueOf(currPos, map)
+            if (value > target)
+                return value.toString()
+            map[currPos] = value
+            if (step == sideLen.toDouble().pow(2).toInt() - sideLen.minus(2).toDouble().pow(2).toInt()) {
+                currPos = Pair(currPos.first+1, currPos.second)
+                sideLen += 2
+                direction = 0
+                step = 1
+            } else {
+                if (step % (sideLen -1) == 0) direction++
+                currPos = Pair(currPos.first + directions[direction].first, currPos.second + directions[direction].second)
+                step++
+            }
         }
-        return ""
     }
 
-    private fun valueOf(currPos: Pair<Int, Int>, map: HashMap<Pair<Int, Int>, Int>): Int {
-        return map.getOrDefault(Pair(currPos.first, currPos.second + 1), 0) +
-                map.getOrDefault(Pair(currPos.first, currPos.second - 1), 0) +
-                map.getOrDefault(Pair(currPos.first - 1, currPos.second + 1), 0) +
-                map.getOrDefault(Pair(currPos.first - 1, currPos.second - 1), 0) +
-                map.getOrDefault(Pair(currPos.first - 1, currPos.second), 0) +
-                map.getOrDefault(Pair(currPos.first + 1, currPos.second + 1), 0) +
-                map.getOrDefault(Pair(currPos.first + 1, currPos.second - 1), 0) +
-                map.getOrDefault(Pair(currPos.first + 1, currPos.second), 0)
+    // Get all the neighbouring cells values, default value is 0 if no value exist
+    private fun valueOf(pos: Pair<Int, Int>, map: HashMap<Pair<Int, Int>, Int>): Int {
+        val value =  map.getOrDefault(Pair(pos.first, pos.second + 1), 0) +
+                map.getOrDefault(Pair(pos.first, pos.second - 1), 0) +
+                map.getOrDefault(Pair(pos.first - 1, pos.second + 1), 0) +
+                map.getOrDefault(Pair(pos.first - 1, pos.second - 1), 0) +
+                map.getOrDefault(Pair(pos.first - 1, pos.second), 0) +
+                map.getOrDefault(Pair(pos.first + 1, pos.second + 1), 0) +
+                map.getOrDefault(Pair(pos.first + 1, pos.second - 1), 0) +
+                map.getOrDefault(Pair(pos.first + 1, pos.second), 0)
+        return if (value > 0) value else 1
     }
 }
---
+++ kotlin/src/aoc207/Day06.kt
@@ -0,0 +1,19 @@
+package aoc207
+
+import util.getInput
+
+fun main() {
+    val input = getInput("201706")
+    println("Part 1: " + Day06().part1(input))
+    println("Part 2: " + Day06().part2(input))
+}
+
+class Day06 {
+    fun part1(input: String): String {
+        return ""
+    }
+
+    fun part2(input: String): String {
+        return ""
+    }
+}
