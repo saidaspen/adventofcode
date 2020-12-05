01bf39c8a4fb8daebf40b826f2592464efdbadf3
Said Aspen <git@saidaspen.se>
2020-12-05 07:01:12 +0100

    polished

--- kotlin/src/aoc2020/Day05.kt
+++ kotlin/src/aoc2020/Day05.kt
@@ -1,8 +1,9 @@
 package aoc2020
 
-import util.*
 import util.P
-import kotlin.math.ceil
+import util.getInput
+import util.x
+import util.y
 
 fun main() {
     println("Part 1: " + Day05.part1())
@@ -10,30 +11,15 @@ fun main() {
 }
 
 object Day05 {
-    private val input = getInput(2020, 5)
+    private val ids = getInput(2020, 5).lines().map { seatOf(it) }.map { it.x * 8 + it.y }.sorted()
 
-    fun part1(): Int = input.lines().map { seatOf(it) }.map { it.x * 8 + it.y }.maxOrNull()!!
+    fun part1(): Int = ids.last()
+
+    fun part2(): Int = (0 until 128 * 8).first { !ids.contains(it) && ids.contains(it + 1) && ids.contains(it - 1) }!!
 
     private fun seatOf(line: String): P<Int, Int> {
-        var rhi = 127
-        var rlo = 0
-        var shi = 7
-        var slo = 0
-        for (c in line.toCharArray()) {
-            when (c) {
-                'F' -> rhi -= (rhi - rlo).cdiv(2)
-                'B' -> rlo += (rhi - rlo).cdiv(2)
-                'L' -> shi -= (shi - slo).cdiv(2)
-                'R' -> slo += (shi - slo).cdiv(2)
-            }
-        }
-        return P(rhi, shi)
-    }
-
-    fun part2(): Int {
-        val seatIds = input.lines().map { seatOf(it) }.map { it.first * 8 + it.second }.toList()
-        return (0 until 128*8).first{ !seatIds.contains(it)&& seatIds.contains(it+1)&& seatIds.contains(it-1) }!!
+        val row = line.substring(0, 7).replace('F', '0').replace('B', '1').toInt(2)
+        val seat = line.substring(7).replace('L', '0').replace('R', '1').toInt(2)
+        return P(row, seat)
     }
 }
-
-typealias P<A, B> = Pair<A, B>
