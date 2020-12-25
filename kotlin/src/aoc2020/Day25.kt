9f734a33432f9e10a04542e46afb9ffaa8842e92
Said Aspen <mail@saidaspen.se>
2020-12-25 06:52:57 +0100

    Aoc 2020 day 25

--- kotlin/src/aoc2020/Day25.kt
+++ kotlin/src/aoc2020/Day25.kt
@@ -1,7 +1,8 @@
 package aoc2020
 
 import util.Day
-import util.HexGridTile
+import util.P
+import util.longs
 
 fun main() {
     Day25.run()
@@ -9,14 +10,29 @@ fun main() {
 
 object Day25 : Day(2020, 25) {
 
-    override fun part1() : Any{
-        println(input)
-        println("Input Length: " + input.length)
-        println("Input #Lines: " + input.lines().size)
-        return ""
+    private const val MOD = 20201227L
+    private const val SUB = 7
+
+    override fun part1(): Any {
+        val (doorKey, cardKey) = P(longs(input)[0], longs(input)[1])
+        val cardLoop = getLoop(doorKey)
+        return encrypt(cardKey, cardLoop)
     }
 
-    override fun part2(): Any {
-        return ""
+    private fun encrypt(subject: Long, loopSize: Long): Any {
+        var value = 1L
+        for (i in 1..loopSize) value = (value * subject) % MOD
+        return value
     }
+
+    private fun getLoop(key: Long): Long {
+        var (value, i) = P(1L, 1L)
+        while (true) {
+            value = (value * SUB) % MOD
+            if (value == key) return i
+            i++
+        }
+    }
+
+    override fun part2() = ""
 }
